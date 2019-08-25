2110517dc8b3800c107bfbc45974d8a13310b4cf
Emily Harber <emily.a.harber@gmail.com>
2019-08-25 15:48:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,19 @@
 # require modules here
+require "yaml"
+require "pry" 
 
-def load_library
+def load_library(lib)
   # code goes here
+  emoticons = YAML.load_file(lib)
+  
+  emotions = {get_meaning: {}, get_emoticon: {}}
+  
+  emoticons.each do |meaning, icons|
+    # emotions[:get_meaning] = meaning
+    # emotions[:get_emoticon] = icons[0]
+    binding.pry 
+  end 
+  emotions 
 end
 
 def get_japanese_emoticon
