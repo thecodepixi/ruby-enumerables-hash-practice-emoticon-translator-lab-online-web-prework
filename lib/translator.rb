df4db25dfbd0ede94d28fce115ef69af16166792
Emily Harber <emily.a.harber@gmail.com>
2019-08-25 15:53:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,8 @@ def load_library(lib)
   emotions = {get_meaning: {}, get_emoticon: {}}
   
   emoticons.each do |meaning, icons|
-    emotions[:get_meaning] = icons[2]
-  
+    emotions[:get_meaning] = icons[1]
+    emotions[:get_emoticon] = icons[0]
   emotions 
 end
 
