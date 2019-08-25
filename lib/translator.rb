fe4cd074c5cafa9a77611b9379b54a8c35df5218
Emily Harber <emily.a.harber@gmail.com>
2019-08-25 16:12:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,8 +22,7 @@ def get_japanese_emoticon(lib, emoticon)
   
   library = load_library(lib)
   
-  if library[:get_emoticon][emoticon]
-    binding.pry 
+  if library[:get_emoticon].has_key?(emoticon) 
   japanese_icon = library[:get_emoticon][emoticon]
   japanese_icon 
   else
